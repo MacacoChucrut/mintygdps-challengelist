ef6653575ed7ec1f1fcfe0854dbd23ec42f03c89
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-05-04 15:03:51 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import cow from './pages/cow.js';
+import Cow from './pages/cow.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/cow', component: cow },
+    { path: '/cow', component: Cow },
 ];
