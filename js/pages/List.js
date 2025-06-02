735d6981a2523791bcde02edc21d7626db410d38
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-06-02 09:11:09 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -25,8 +25,7 @@ export default {
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
-                            <p v-if="i + 1 <= 50" class="type-label-lg">#{{ i + 1 }}</p>
-                            <p v-else class="type-label-lg">Legacy</p>
+                        <p class="type-label-lg" :style="{ color: i + 1 > 150 ? 'darkgrey' : 'inherit' }"> #{{ i + 1 }}</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
                             <button @click="selected = i">
@@ -54,7 +53,7 @@ export default {
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
                     <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
-                    <p v-else>This level does not accept new records.</p>
+                    <p v-else>This level has fallen into Legacy and no longer accepts new records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
                             <td class="percent">
