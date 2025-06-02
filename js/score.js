de784ca596c48b8cd4139677eee6f84939512651
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-06-02 09:40:36 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,15 +11,12 @@ const scale = 2;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
-        return 0;
-    }
-    if (rank > 50 && percent < 100) {
+    if (rank > 150) {
         return 0;
     }
 
-   // Fórmula lineal: puesto 1 → 200, puesto 100 → 1
-    let score = -2.0101 * rank + 202.0101;
+   // Fórmula lineal: puesto 1 → 150, puesto 100 → 1
+    let score = -1 * rank + 151;
 
     score *= ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     score = Math.max(0, score);
