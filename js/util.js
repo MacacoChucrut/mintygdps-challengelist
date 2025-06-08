61454f4035454975771a7ec3de7d17fec3b86cc4
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-06-08 15:27:22 -0400

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,46 +1,33 @@
-// Función robusta para extraer ID de YouTube
+// https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
 export function getYoutubeIdFromUrl(url) {
-    const regex = /(?:youtube\.com\/(?:.*v=|embed\/)|youtu\.be\/)([^&?/]+)/;
-    const match = url.match(regex);
-    return match ? match[1] : null;
+    return url.match(
+        /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/,
+    )?.[1] ?? '';
 }
 
-// Generar el iframe correcto según la URL
-export function embed(url) {
-    if (url.includes("youtube.com") || url.includes("youtu.be")) {
-        const id = getYoutubeIdFromUrl(url);
-        if (id) {
-            return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen></iframe>`;
-        }
-    } else if (url.includes("medal.tv")) {
-        const match = url.match(/medal\.tv\/(?:clip|games\/\w+\/clips)\/([a-zA-Z0-9]+)/);
-        const id = match ? match[1] : null;
-        if (id) {
-            return `<iframe width="560" height="315" src="https://medal.tv/clip/${id}/embed" frameborder="0" allowfullscreen allow="autoplay; fullscreen"></iframe>`;
-        }
-    }
-
-    // Si no se pudo embeber correctamente, usar enlace directo
-    return `<a href="${url}" target="_blank" rel="noopener noreferrer">View Video</a>`;
+export function embed(video) {
+    return `https://www.youtube.com/embed/${getYoutubeIdFromUrl(video)}`;
 }
 
-// Otros utilitarios existentes
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 3 });
+    return num.toLocaleString(undefined, { minimumFractionDigits: 2 });
 }
 
 export function getThumbnailFromId(id) {
     return `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
 }
 
-// Mezclar un arreglo aleatoriamente
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export function shuffle(array) {
     let currentIndex = array.length, randomIndex;
 
+    // While there remain elements to shuffle.
     while (currentIndex != 0) {
+        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
+        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex],
             array[currentIndex],
