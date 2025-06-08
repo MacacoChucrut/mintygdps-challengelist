36a96f58f92af58c21ec09c8b8c58c1d8235b8fd
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-06-08 14:13:24 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -105,6 +105,9 @@ export default {
                         Video proof is required for Top 20 Challenges.
                     </p>
                     <p>
+                        Completions or Verifications Must be uploaded in the format of a youtube video.
+                    </p>
+                    <p>
                         Cheat indicator is required if a modmenu with the feature is being used.
                     </p>
                     <p>
