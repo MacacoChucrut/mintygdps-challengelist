90e93c07872b071d012e557fc38aaa5a2b9024c8
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-11-04 13:46:04 -0300

    Create Pending.js

---
+++ js/pages/Pending.js
@@ -0,0 +1,7 @@
+import { fetchList } from '../content.js';
+import { getThumbnailFromId, getYoutubeIdFromUrl, shuffle } from '../util.js';
+
+import Spinner from '../components/Spinner.js';
+import Btn from '../components/Btn.js';
+
+export default {
