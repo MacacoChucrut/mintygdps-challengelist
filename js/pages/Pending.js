bb6163d616a5eae86d419c1c498da1d175f0a837
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-11-04 17:02:32 -0300

    Delete js/pages/Pending.js

--- js/pages/Pending.js
+++
@@ -1,7 +0,0 @@
-import { fetchList } from '../content.js';
-import { getThumbnailFromId, getYoutubeIdFromUrl, shuffle } from '../util.js';
-
-import Spinner from '../components/Spinner.js';
-import Btn from '../components/Btn.js';
-
-export default {
